385dae0717765d960eaf1087690822a3751c1af8
reza4096 <reza4096@yahoo.com>
2022-12-02 04:18:35 +0330

    args definition

--- args.py
+++ args.py
@@ -1,6 +1,10 @@
+# The special syntax *args
+# in function definitions in python is used to pass a variable number of arguments to a function.
+# It is used to pass a non-key worded,
+# variable-length argument list. 
+
 def myFun(*args):
     for arg in args:
         print(arg)
- 
- 
+
 myFun('Hello', 'python', 'is', 'fun')
