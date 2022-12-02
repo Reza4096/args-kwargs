e5cba0a8b17d09eb7658a28c34f16a18004349ef
reza4096 <reza4096@yahoo.com>
2022-12-02 04:20:45 +0330

    to illustrate *args with a first extra argument

--- args.py
+++ args.py
@@ -8,3 +8,14 @@ def myFun(*args):
         print(arg)
 
 myFun('Hello', 'python', 'is', 'fun')
+
+
+# to illustrate *args with a first extra argument
+
+def myFun2(arg1, *argv):
+    print("First argument :", arg1)
+    for arg in argv:
+        print("Next argument through *argv :", arg)
+ 
+ 
+myFun2('Hello', 'python', 'is', 'fun')
