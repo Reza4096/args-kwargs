59db40c16d3fb0e8e57ab8585c68094eea05c243
reza4096 <reza4096@yahoo.com>
2022-12-02 04:34:55 +0330

    one extra argument

--- kwargs.py
+++ kwargs.py
@@ -6,3 +6,10 @@ def myFun(**kwargs):
         print("%s == %s" % (key, value))
 
 myFun(first='Reza', mid='Was', last='Here')
+
+# to illustrate  **kwargs for a variable number of keyword arguments with one extra argument
+def myFun2(arg1, **kwargs):
+	for key, value in kwargs.items():
+		print("%s == %s" % (key, value))
+
+myFun2("Hi", first='Reza', second='Was', third='Here')
